9a8b746bfd4686625a13c208a96a21d1b54aa39b
Jack Amos <jack671guam@yahoo.com>
2019-10-23 17:56:53 -0600

    Linked login script and index
    Should be able to be tested now, might fail

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 $connection = mysql_connect("localhost", "root", ""); // Establishing connection with server..
-$db = mysql_select_db("college", $connection); // Selecting Database.
+$db = mysql_select_db("users", $connection); // Selecting Database.
 $email=$_POST['email1']; // Fetching Values from URL.
 $password= sha1($_POST['password1']); // Password Encryption, If you like you can also leave sha1.
 // check if e-mail address syntax is valid or not
